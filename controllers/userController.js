1d0531ed16d3065417650b43de3c9d3ec03092e4
ditaisyiyah <ditalara19@gmail.com>
2021-08-24 07:14:58 +0700

    add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const { checkPassword } = require('../helpers/bcrypt');
+const { generateToken } = require('../helpers/jwt');
+const { User } = require('../models');
+
+class userController{
+  static async register(req, res, next){
+    try {
+      const { email, password } = req.body;
+      const user = await User.create({ email, password })
+
+      const access_token = generateToken({ 
+        email: user.email, 
+        password: user.password 
+      });
+      res.status(201).json({ access_token });
+
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async login(req, res, next){
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if(!user) throw ({ name: 'InvalidLogin' });
+
+      const isPasswordValid = checkPassword(password, user.password);
+      if(!isPasswordValid) throw ({ name: 'InvalidLogin' });
+
+      const access_token = generateToken({ 
+        email: user.email, 
+        password: user.password 
+      });
+      res.status(200).json({ access_token });
+
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController;
