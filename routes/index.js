ffe8556e89da3e588c1e52cd73f117db7a4fc9bd
ditaisyiyah <ditalara19@gmail.com>
2021-08-26 08:40:01 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 const nasaController = require('../controllers/nasaController');
 const userController = require('../controllers/userController');
+const newsController = require('../controllers/newsController');
 const aneoQuery = require('../middlewares/aneoQuery');
 const apodQuery = require('../middlewares/apodQuery');
+const newsQuery = require('../middlewares/newsQuery');
 const authentication = require('../middlewares/authentication');
 
 const router = require('express').Router();
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/aneo', aneoQuery, nasaController.fetchAsteroid);
 
 router.use(authentication);
 
+router.get('/profile', userController.getProfile);
 router.get('/apod', apodQuery, nasaController.fetchPicture);
-router.get('/aneo', aneoQuery, nasaController.fetchAsteroid);
 router.get('/news', newsQuery, newsController.fetchNews);
 
 
