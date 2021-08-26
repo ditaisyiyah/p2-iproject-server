99cc759ec98e97daa27f64f33f8cad1bb4fb9052
ditaisyiyah <ditalara19@gmail.com>
2021-08-26 08:54:23 +0700

    integrate nodemailer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const { checkPassword } = require('../helpers/bcrypt');
+const getMessage = require('../helpers/email');
 const { generateToken } = require('../helpers/jwt');
+const transporter = require('../helpers/nodemailer');
 const { User } = require('../models');
 
 class userController{
@@ -12,11 +14,24 @@ class userController{
       const user = await User.create({ email, password })
 
       const access_token = generateToken({ 
+        id: user.id,
         email: user.email, 
         password: user.password 
       });
-      res.status(201).json({ access_token });
 
+      console.log('---------asteroid siap dikirim ke email user', asteroid);
+      const payload = { email: user.email, asteroid }
+      const message = getMessage(payload)
+
+      transporter.sendMail(message, (err, info) => {
+        // console.log('----------err send email', err, info);
+        if(err) {
+          throw ({ name: 'FailedSendEmail' }) // TODO cari library utk auth email
+        }
+      });
+
+      res.status(201).json({ access_token });
+      
     } catch (err) {
       next(err)
     }
@@ -24,7 +39,7 @@ class userController{
 
   static async login(req, res, next){
     try {
-      const { email, password } = req.body;
+      const { email, password, asteroid } = req.body;
       const user = await User.findOne({ where: { email } });
       if(!user) throw ({ name: 'InvalidLogin' });
 
@@ -32,13 +47,36 @@ class userController{
       if(!isPasswordValid) throw ({ name: 'InvalidLogin' });
 
       const access_token = generateToken({ 
+        id: user.id,
         email: user.email, 
         password: user.password 
       });
-      res.status(200).json({ access_token });
 
+      console.log('---------asteroid siap dikirim ke email user', asteroid);
+      const payload = { email: user.email, asteroid }
+      const message = getMessage(payload)
+
+      transporter.sendMail(message, (err, info) => {
+        // console.log('----------err send email', err, info);
+        if(err) {
+          throw ({ name: 'FailedSendEmail' }) // TODO cari library utk auth email
+        }
+      });
+      
+      res.status(200).json({ access_token });
+      
     } catch (err) {
+      console.log(err.message);
       next(err)
+    }
+  }
+
+  static async getProfile(req, res, next){
+    try {
+      const { id, email } = req.user;
+      res.status(200).json({ id, email });
+    } catch (err) {
+      next(err);
     }
   }
 }
---
+++ helpers/email.js
@@ -0,0 +1,19 @@
+function getMessage(payload){
+  const { email, asteroid } = payload;
+  const { count, name, distance, closestAt, velocity, isHazardous, diameter, url } = asteroid;
+  return  {
+    from: '"UNIVERSE CODE"<no-reply@universecode.com>',
+    to: email,
+    subject: "An Asteroid Close Approach You! ",
+    html: `
+      <p>There are <b>${count} asteroids</b> close approach you in total</p>
+      <p>The closest one is named <b>${name}</b>, at distance <b>${distance.toFixed(4)} Astronomical Unit</b></p>
+      <p>Its velocity is <b>${Number(velocity).toFixed(4)} kilometers per hour</b></p>
+      <p>It is estimated having diameter <b>${diameter.toFixed(4)} kilometers</b></p>
+      <p>This asteroid is <b>${isHazardous? '': 'not'} hazardous</b></p>
+      <p>For further information: <b>${url}</b></p>
+    `
+  };
+}
+
+module.exports = getMessage;
