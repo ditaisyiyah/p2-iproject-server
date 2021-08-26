f3f5fca55fb00f1878d92bf067efd0c954d90ac2
ditaisyiyah <ditalara19@gmail.com>
2021-08-26 08:49:49 +0700

    update req res

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -11,6 +11,13 @@ class newsController{
 
       if(news.status === 'error') throw ({ name: 'FailedNews' });
 
+      news.articles.forEach((article, index) => {
+        article.id = index + 1;
+        article.source = article.source.name;
+        article.publishedAt = article.publishedAt.slice(0, article.publishedAt.indexOf('T'))
+        delete article.source;
+      })
+
       res.status(200).json(news.articles)
 
     } catch (err) {
