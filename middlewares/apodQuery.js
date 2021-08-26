326cc3088fc52f47f505b5466822e2ecca5090b6
ditaisyiyah <ditalara19@gmail.com>
2021-08-26 08:47:05 +0700

    update req res

--- middlewares/apodQuery.js
+++ middlewares/apodQuery.js
@@ -4,16 +4,22 @@ const nasaAPIKey = process.env.NASA_API_KEY;
 // Query Paramaters from APOD API:
 // date YYYY-MM-DD, default today
 // start_date YYYY-MM-DD, default none
-// end_date YYYY-MM-DD, default today
+// end_date YYYY-MM-DD, default today (not needed)
 
 function apodQuery(req, res, next) {
   try {
-    const { date, start_date, end_date } = req.query;
+    let { date, start_date } = req.query;
     
     let query = '';
-    if(date) query += `&date=${date}`;
-    if(start_date) query += `&start_date=${start_date}`;
-    if(end_date) query += `&end_date=${end_date}`;
+    if(date && !start_date){
+      date = date.slice(0, date.indexOf('T'))
+      query += `&date=${date}`;
+      req.type = 'object'
+    }else if(!date && start_date){
+      start_date = start_date.slice(0, start_date.indexOf('T'))
+      query += `&start_date=${start_date}`;
+      req.type = 'array'
+    }
 
     req.url = (`${apodURL}${nasaAPIKey}${query}`);
     
